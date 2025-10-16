b7ccc56b44487be116a1cfa5553208ce70020104
Fuji-Vanilli <ijufillivan@gmail.com>
2025-10-16 16:17:33 +0300

    update

--- sd-front/app/(auth)/login/layout.tsx
+++ sd-front/app/(auth)/login/layout.tsx
@@ -1,9 +1,32 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.png";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="relative flex min-h-svh flex-col items-center justify-center">
             <div className="flex w-full max-w-sm flex-col gap-6">
+                <Link href="/" className={buttonVariants({
+                    variant: "outline",
+                    className: "absolute top-4 left-4"
+                })}>
+                    <ArrowLeft className="s-4" />
+                    Back
+                </Link>
+                <div className="flex w-full max-w-sm flex-col gap-6">
+                    <Link href="/" className="flex items-center gap-2 self-center font-medium">
+                        <Image src={Logo} alt="Sikajy Dimy" width={32} height={32}/>
+                        Sikajy Dimy
+                    </Link>
+                </div>
                 {children}
+
+                <div className="text-balance text-center text-xs text-muted-foreground">
+                    By clicking continue, you agree to our <span className="hover:text-primary hover:underline">Terms of service </span>
+                    and <span className="hover:text-primary hover:underline">Privacy policy</span>.
+                </div>
             </div>
         </div>
     );
