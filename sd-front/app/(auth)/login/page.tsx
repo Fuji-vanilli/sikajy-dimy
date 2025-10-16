015f652dc45f35c53c1503efe8106e6b7306867c
Fuji-Vanilli <ijufillivan@gmail.com>
2025-10-16 15:04:31 +0300

    update

--- sd-front/app/(auth)/login/page.tsx
+++ sd-front/app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function LoginPage() {
   return (
-    <div>
-      <h1 className="">Login</h1>
-      <Button>Login with GitHub</Button>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Welcome back!</CardTitle>
+        <CardDescription>Login to your Github or Email account</CardDescription>
+      </CardHeader>
+    </Card>
   );
 }
