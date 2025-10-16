b150cba4fc2c07cef151d3780a1231f55218bed5
Fuji-Vanilli <ijufillivan@gmail.com>
2025-10-16 14:58:37 +0300

    first commit front

---
+++ sd-front/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1 className="">Login</h1>
+      <Button>Login with GitHub</Button>
+    </div>
+  );
+}
