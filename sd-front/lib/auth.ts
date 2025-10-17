a9c0b8123151a906235daa9774ca7200602396c3
Fuji-Vanilli <ijufillivan@gmail.com>
2025-10-17 07:54:20 +0300

    update

--- sd-front/lib/auth.ts
+++ sd-front/lib/auth.ts
@@ -1,6 +1,11 @@
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 export const auth = betterAuth({
-  secret: process.env.BETTER_AUTH_SECRET,
-  url: process.env.BETTER_AUTH_URL,
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
 });
