67c7cb07848850e8fc728cd1429cfe854c74d5b0
Fuji-Vanilli <ijufillivan@gmail.com>
2025-10-17 11:25:10 +0300

    update

--- sd-front/app/page.tsx
+++ sd-front/app/page.tsx
@@ -3,11 +3,21 @@
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
   const { data: session } = authClient.useSession();
   async function signOut() {
-    await authClient.signOut();
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/login");
+          toast.success("Successfully signed out, you will be redirected to login page.");
+        }
+      }
+    });
   }
 
   return (
@@ -16,7 +26,7 @@ export default function Home() {
       {session ? (
         <div>
           <p>{session.user.name}</p>
-          <Button onClick={signOut}>Sign Out</Button>
+          <Button onClick={ signOut }>Sign Out</Button>
         </div>
       ): <Button>Login</Button>}
     </div>
