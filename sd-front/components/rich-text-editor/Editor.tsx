796a08a515ab013f32e2920f98900a05bb9ef81e
Fuji-Vanilli <ijufillivan@gmail.com>
2025-10-20 14:37:24 +0300

    update

--- sd-front/components/rich-text-editor/Editor.tsx
+++ sd-front/components/rich-text-editor/Editor.tsx
@@ -5,7 +5,7 @@ import StarterKit from "@tiptap/starter-kit";
 import { Menubar } from "./Menubar";
 import TextAlign from "@tiptap/extension-text-align";
 
-export function RichTextEditor() {
+export function RichTextEditor({field} : {field: any}) {
     const editor = useEditor({
         extensions: [
             StarterKit,
@@ -15,8 +15,12 @@ export function RichTextEditor() {
         ],
         editorProps: {
             attributes: {
-                class: "min-h-[300px] p-4"
+                class: "min-h-[300px] p-4 focus:outline-none prose prose-sm sm:prose lg:prose-lg xl:prose-xl dark:prose-invert !w-full !max-w-none",
             }
+        },
+        onUpdate: ({ editor }) => {
+
+            field.onChange(JSON.stringify(editor.getJSON()));
         },
         immediatelyRender: false,
     })
